c6ffed8dd245b9d13b46519bb03df838289721ad
aantoine <antoineagustin@gmail.com>
2015-11-27 13:04:58 -0300

    added GetAllRequest

--- src/request/GetAllRequest.java
+++ src/request/GetAllRequest.java
@@ -1,10 +1,15 @@
 package request;
 
+import interfaces.Request;
+
 import java.io.IOException;
 
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import variables.IntegerResSet;
 import variables.ResSet;
 import builder.RequestBuilder;
 import error.ResSetException;
@@ -13,21 +18,38 @@ public class GetAllRequest extends ExcelRequest{
 
 	public GetAllRequest(ResSet input, String path) throws IOException {
 		super(input, path);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public ResSet execute() throws ResSetException {
-		// TODO Auto-generated method stub
-		return null;
+		
+		Sheet sheet = this.getSheet();
+		int count = 0;
+        for (@SuppressWarnings("unused") Row row : sheet) {
+            count ++;
+        }
+		
+		return new IntegerResSet(count);
 	}
 
 	@Override
 	public ResSet getInputFromRawData(JSONObject rawData,
 			RequestBuilder requestBuilder) throws JSONException,
 			ResSetException {
-		// TODO Auto-generated method stub
+		if(rawData.getJSONObject("sheet").getString("type").equals("string")){
+			this.setSheet(rawData.getJSONObject("sheet").getString("var"));
+		}
+		else{
+			this.setSheet(rawData.getJSONObject("sheet").getInt("var"));
+		}
+		
 		return null;
+	}
+	
+	public static void main(String[] args) throws IOException, ResSetException {
+		Request r = new GetAllRequest(null, "ejemplo.xlsx");
+		ResSet set = r.execute();
+		System.out.println(set);
 	}
 
 }
